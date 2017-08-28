fe64402ee3cb63fb2153e24e4571dbee6ca2c288
Hiroki Noda <kubo39@gmail.com>
2017-08-28 23:47:55 +0900

    Add missing semihosting SYS_XXX

--- source/semihosting.d
+++ source/semihosting.d
@@ -16,10 +16,33 @@ version (LDC)
     pragma(LDC_no_typeinfo);
 }
 
+//  semihosting: http://www.keil.com/support/man/docs/armcc/armcc_pge1358787045051.htm
 enum SYS : uint
 {
-    OPEN = 1,
-    WRITE = 5,
+    OPEN = 0x01,
+    CLOSE = 0x02,
+    WRITEC = 0x03,
+    WRITE0 = 0x04,
+    WRITE = 0x05,
+    READ = 0x06,
+    READC = 0x07,
+    ISERROR = 0x08,
+    ISTTY = 0x09,
+    SEEK = 0x0A,
+    FLEN = 0x0C,
+    TMPNAM = 0x0D,
+    REMOVE = 0x0E,
+    RENAME = 0x0F,
+    CLOCK = 0x10,
+    TIME = 0x11,
+    SYSTEM = 0x12,
+    ERRNO = 0x13,
+    GET_CMDLINE = 0x15,
+    HEAPINFO = 0x16,
+    ENTERSVC = 0x17,
+    REPORT_EXCEPTION = 0x18,
+    ELAPSED = 0x30,
+    TICKFREQ = 0x31
 }
 
 enum MODE : uint
