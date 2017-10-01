5a9764952c92d8169319abb292397fefff41c36f
Hiroki Noda <kubo39@gmail.com>
2017-10-01 14:56:00 +0900

    Add ddoc and update open mode.

--- source/semihosting.d
+++ source/semihosting.d
@@ -1,3 +1,6 @@
+/**
+Semihosting for ARM Cortex-M processors.
+ */
 module semihosting;
 
 version (LDC)
@@ -15,7 +18,13 @@ version (LDC)
     pragma(LDC_no_typeinfo);
 }
 
-//  semihosting: http://www.keil.com/support/man/docs/armcc/armcc_pge1358787045051.htm
+
+/**
+Semihosting's operations.
+
+See_also:
+  semihosting: http://www.keil.com/support/man/docs/armcc/armcc_pge1358787045051.htm
+ */
 enum SYS : uint
 {
     OPEN = 0x01,
@@ -44,28 +53,59 @@ enum SYS : uint
     TICKFREQ = 0x31
 }
 
+
+/**
+open syscall mode params.
+ */
 enum MODE : uint
 {
+    R = 0,
+    R_BINARY = 1,
+    RW = 2,
+    RW_BINARY = 3,
     W_TRUNC = 4,
+    W_TRUNC_BINARY = 5,
+    RW_TRUNC = 6,
+    RW_TRUNC_BINARY = 7,
+    W_APPEND = 8,
+    W_APPEND_BINARY = 9,
+    RW_APPEND = 10,
+    RW_APPEND_BINARY = 11,
 }
 
+
+/**
+Call semihosting's operations.
+ */
 int syscall(uint nr, const uint* arg) pure
 {
     return __asm!int("bkpt 0xAB", "={r0}, ~{r0}, ~{r1}", nr, arg);
 }
 
+
+/**
+Open file $(D name) with mode $(mode).
+ */
 int open(in string name, MODE mode) pure
 {
     const uint[3] arg = [cast(uint) name.ptr, mode, name.length - 1];
     return syscall(SYS.OPEN, arg.ptr);
 }
 
+
+/**
+Writes its arguments to host.
+*/
 int write(int fd, in ubyte* ptr, uint len) pure
 {
     const uint[3] arg = [cast(uint) fd, cast(uint) ptr, len];
     return syscall(SYS.WRITE, arg.ptr);
 }
 
+
+/**
+Open host's standard output and writes its arguments to.
+*/
 void writeBuffer(in ubyte[] buffer)
 {
     auto fd = open(":tt\0", MODE.W_TRUNC);
@@ -84,11 +124,17 @@ void writeBuffer(in ubyte[] buffer)
     }
 }
 
+
+/// ditto
 void write(in string s)
 {
     writeBuffer(cast(const ubyte[]) s);
 }
 
+
+/**
+Writes its arguments to host, followed by a newline.
+*/
 void writeln(in string s)
 {
     writeBuffer(cast(const ubyte[]) s);
